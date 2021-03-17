e2df24fcae4f7085fcd756d23020836df38dd0df
= <=>
2021-03-17 08:14:48 +0700

    add dependencies

---
+++ src/main/java/com/codegym/lastwhisper/model/BaseEntity.java
@@ -0,0 +1,23 @@
+package com.codegym.lastwhisper.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.data.annotation.CreatedBy;
+
+import java.time.ZonedDateTime;
+
+public abstract class BaseEntity {
+    @JsonIgnore
+    @CreatedBy
+    private String createBy;
+
+    @JsonIgnore
+    @CreatedBy
+    private ZonedDateTime createDate;
+
+    @JsonIgnore
+    @CreatedBy
+    private String updateBy;
+
+    @JsonIgnore
+    private String version;
+}
