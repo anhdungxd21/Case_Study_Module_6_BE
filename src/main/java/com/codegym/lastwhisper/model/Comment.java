17955e9d1f0ab6883bf3314ae5a9947e7b690d84
luonganhtu19 <luonganhtu1995@gmail.com>
2021-03-18 11:23:51 +0700

    'vrs1-dev'

--- src/main/java/com/codegym/lastwhisper/model/Comment.java
+++ src/main/java/com/codegym/lastwhisper/model/Comment.java
@@ -1,36 +1,21 @@
 package com.codegym.lastwhisper.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedBy;
-import javax.persistence.Id;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table
 public class Comment extends BaseEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String content;
     private Long idUser;
     private Long idType;
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
 }
